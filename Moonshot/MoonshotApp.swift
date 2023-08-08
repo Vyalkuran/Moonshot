4c46e58a8e1d30a6fde1c26ecb1a6a099c3100d7
sebastian.popa <sebastian.popa@softvision.com>
2023-08-08 19:07:52 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by sebastian.popa on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
