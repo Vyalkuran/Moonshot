845d228b9a79313f33cf4545d6c9efbde10212d8
sebastian.popa <sebastian.popa@softvision.com>
2023-08-10 17:39:44 +0300

    Project 8 implementation from 100 days of SwiftUI

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by sebastian.popa on 8/9/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
